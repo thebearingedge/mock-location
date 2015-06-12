1ed2d4763f64228bec0e3924a7e37d9bff513a97
Tim Davis <thebearingedge@gmail.com>
2015-06-12 13:45:30 -0700

    basic features

--- MockLocation.js
+++ MockLocation.js
@@ -1,6 +1,46 @@
 
 'use strict';
 
-module.exports = function () {
+var parse = require('url-parse');
 
+module.exports = function mockLocation(url) {
+
+  var location = parse(url);
+
+  var api = {
+    replace: function (url) {
+      location = parse(url);
+    },
+    toString: function () {
+      return location.toString();
+    }
+  };
+
+  Object.defineProperties(api, {
+
+    hash: {
+      get: function () {
+        return location.hash;
+      },
+      set: function (hash) {
+        if (hash[0] !== '#') {
+          hash = '#' + hash;
+        }
+        location.set('hash', hash);
+      }
+    },
+
+
+    href: {
+      get: function () {
+        return location.href;
+      },
+      set: function (href) {
+        api.replace(href);
+      }
+    }
+
+  });
+
+  return api;
 };
--- test/MockLocation.test.js
+++ test/MockLocation.test.js
@@ -3,15 +3,73 @@
 
 var expect;
 var chai = require('chai');
-var sinon = require('sinon');
-var sinonChai = require('sinon-chai');
-
-chai.use(sinonChai);
+var mockLocation = require('../mockLocation');
 
 expect = chai.expect;
 
-describe('index', function () {
+describe('mockLocation', function () {
 
-  it('should run tests');
+  var location;
+
+  beforeEach(function () {
+    location = mockLocation('http://example.com/foo?bar=baz#qux');
+  });
+
+
+  describe('#replace()', function () {
+
+    it('should replace the url', function () {
+
+      location.replace('http://example.com');
+
+      expect(location.toString()).to.equal('http://example.com');
+    });
+
+  });
+
+
+  describe('#hash', function () {
+
+    it('should read the hash portion of the url', function () {
+
+      expect(location.hash).to.equal('#qux');
+    });
+
+
+    it('should set the hash portion of the url', function () {
+
+      location.hash = '#quux';
+
+      expect(location.hash).to.equal('#quux');
+    });
+
+
+    it('should set the hash portion of the url without "#"', function () {
+
+      location.hash = 'quux';
+
+      expect(location.hash).to.equal('#quux');
+    });
+
+  });
+
+
+  describe('#href', function () {
+
+    it('should read the full url', function () {
+
+      expect(location.href).to.equal('http://example.com/foo?bar=baz#qux');
+    });
+
+
+    it('should replace the internal location instance', function () {
+
+      location.href = 'http://foo.com';
+
+      expect(location.href).to.equal('http://foo.com');
+      expect(location.hash).to.equal('');
+    });
+
+  });
 
 });
