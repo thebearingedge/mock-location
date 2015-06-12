e930fee4469a2223f79d6a62c8492c228b4ae322
Tim Davis <thebearingedge@gmail.com>
2015-06-11 21:41:11 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+'use strict';
+
+module.exports = function () {
+  if (arguments.length > 0) {
+    return false;
+  }
+  return true;
+};
---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+
+'use strict';
+
+var expect;
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+
+expect = chai.expect;
+
+describe('index', function () {
+
+  it('should run tests');
+
+});
